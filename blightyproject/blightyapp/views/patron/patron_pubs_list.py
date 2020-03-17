f5e9c3e098127ca4978c75fb3eb6636b0f1d617c
Schatfield <dgoldmine@icloud.com>
2020-03-17 11:46:57 -0500

    clear git cache

--- blightyproject/blightyapp/views/patron/patron_pubs_list.py
+++ blightyproject/blightyapp/views/patron/patron_pubs_list.py
@@ -2,6 +2,13 @@ import sqlite3
 from django.shortcuts import redirect, render, reverse
 from blightyapp.models import PatronPub, Pub
 
+
+""" 
+
+This view is what controls what happens when a user clicks wished/visited on region_details form 
+
+"""
+
 def patron_pub_list(request):
     if request.method == 'GET':
         
@@ -18,10 +25,17 @@ def patron_pub_list(request):
         # }
 
         # return render(request, template, context)
-    elif request.method == 'POST':
-        print("POST!!!!!!!!!")
+    elif request.method == 'POST':        
         form_data = request.POST
         
+        """ 
+            To do: assign the value of the patron_pub_id input field from the hidden form to a variable
+            Conditionals
+            First: we have to check if the value of patron_pub_id is none or some sort of empty string- print to see what is return to define this. 
+            If it's empty, it is a post (nothing to PUT yet) - POST functionality is currently working
+            Else we are gonna make a put - PUT needs to be written
+        
+        """
         # instantiate...
         new_patron_pub = PatronPub(
             patron_id = request.user.patron.id,
