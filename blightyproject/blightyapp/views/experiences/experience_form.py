831c58fa8a7463c4c3a607aed265a33573104e38
Schatfield <dgoldmine@icloud.com>
2020-03-19 20:48:37 -0500

    fully fleshed out account templates with visited, wished, and add experience functionality. Edit and Delete of Experiences now functional

--- blightyproject/blightyapp/views/experiences/experience_form.py
+++ blightyproject/blightyapp/views/experiences/experience_form.py
@@ -1,5 +1,5 @@
 import sqlite3
-from django.shortcuts import render
+from django.shortcuts import redirect, render, reverse
 # from django.contrib.auth.decorators import login_required
 from blightyapp.models import PatronPub
 from blightyapp.models import Pub
@@ -11,8 +11,10 @@ def get_patron_pub(patron_pub_id):
 
 # @login_required
 def experience_form(request, patron_pub_id):
+    # this is getting the patron_pub record form the data and is available to the whole function
+    patron_pub = get_patron_pub(patron_pub_id)   
+
     if request.method == 'GET':
-        patron_pub = get_patron_pub(patron_pub_id)        
         
         template = 'experiences/form.html'
         context = {
@@ -21,18 +23,38 @@ def experience_form(request, patron_pub_id):
 
         return render(request, template, context)
 
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        # Check if this POST is for deleting a book
+        #
+        # Note: You can use parenthesis to break up complex
+        #       `if` statements for higher readability
+        if (
+            "actual_method" in form_data
+            and form_data["actual_method"] == "DELETE"
+        ):
+            patron_pub.has_experience = False
+            patron_pub.beers_tried = ""
+            patron_pub.food_ate = ""
+            patron_pub.experience = ""
+
+            # saving updated patron_pub record with the added experience
+            patron_pub.save()
+
+            return redirect(reverse('blightyapp:experience_list'))
+
 # @login_required
-# def book_edit_form(request, book_id):    
+def experience_edit_form(request, patron_pub_id):  
 
-#     if request.method == 'GET':
-#         book = get_book(book_id)
-#         print(book)
-#         libraries = get_libraries()
+    if request.method == 'GET':
+        patron_pub = get_patron_pub(patron_pub_id)     
+        print(patron_pub)
 
-#         template = 'books/form.html'
-#         context = {
-#             'book': book,
-#             'all_libraries': libraries
-#         }
+        template = 'experiences/form.html'
+        context = {
+            
+            'patron_pub': patron_pub
+        }
 
-#         return render(request, template, context)
+        return render(request, template, context)
