0820c4c33ad6f337f30fb6ce75a354b4442902d3
Schatfield <dgoldmine@icloud.com>
2020-03-21 22:33:30 -0500

    home template and conditional routing for nav bar an icons complete and functioning

--- blightyproject/blightyapp/urls.py
+++ blightyproject/blightyapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -9,7 +11,7 @@ app_name = "blightyapp"
 urlpatterns = [
     path('home', home, name='home'),
     path('regions/', region_list, name='region_list'),
-    path('regions/<int:region_id>/', region_details, name='region'),
+    path('regions/<int:region_id>/', region_details, name='region_details'),
     path('experiences/', experience_list, name='experience_list'),
     path('experiences/<int:patron_pub_id>/form/', experience_form, name='experience_form'),
     path('experiences/<int:patron_pub_id>/form/', experience_edit_form, name='experience_edit_form'),
@@ -19,4 +21,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', register_user, name="register"),
     path('logout/', logout_user, name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blightyproject/blightyapp/views/patrons/patron_pubs_list.py
+++ blightyproject/blightyapp/views/patrons/patron_pubs_list.py
@@ -12,13 +12,15 @@ This view is what controls what happens when a user clicks wished/visited button
 @login_required
 def patron_pub_list(request):
 
+    print(request)
+
     if request.method == 'POST':        
         form_data = request.POST
 
         # [patron_pub_id] is the name in the form field and that is how it is getting the value. Since we are getting this value from the form we follow this pattern to declare the variable. it's just like the new_patron_pub variable below that is also getting the value for each key from the name we gave it in the form. Also, form_data is an object. it's holding all the request data from the form as an object and we are storing that object in the variable named form_data.
         patron_pub_id = form_data['patron_pub_id']
-        print('PPID!!!!', patron_pub_id)
-
+        # Getting region id off form, using it to redirect user to correct region page
+        region_id = form_data['region_id']
 
         # this is saying if there is not a patron pub id present, post the request made by way of a form data object and construct it like this
         if not patron_pub_id:
@@ -27,7 +29,7 @@ def patron_pub_list(request):
                 pub_id = form_data['pub_id'],
                 is_wished = form_data['is_wished'],
                 is_visited = form_data['is_visited'],
-                has_experience = False,
+                has_experience = False, 
                 beers_tried = "",
                 food_ate = "",
                 experience = ""
@@ -36,19 +38,28 @@ def patron_pub_list(request):
             new_patron_pub.save()
             
         else: 
+            
             patron_pub = PatronPub.objects.get(id=patron_pub_id)   
             
+            # Which button was clicked, wished or visited?
             is_wished_button = form_data['is_wished'] == "True"
             
             if is_wished_button:
                 patron_pub.is_wished = not patron_pub.is_wished 
-
             else:
                 patron_pub.is_visited = not patron_pub.is_visited
-                
+            
             patron_pub.save()
-       
-    return redirect(reverse('blightyapp:home'))
+                
+            # Use context input field to determine where in the app the user click wished or visited buttons
+            # the name on the form can be used as a property on form_data
+        context = form_data['context']
+        if context == 'wish_list':
+            return redirect('blightyapp:wish_list')
+        elif context == 'visited_list':
+            return redirect('blightyapp:visited_list')
+        else:
+            return redirect('blightyapp:region_details', region_id=region_id)
 
     # """ 
     #         To do: assign the value of the patron_pub_id input field from the hidden form to a variable
